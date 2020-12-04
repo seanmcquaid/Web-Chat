10d34557e31f41aeebfda39cd7de45b309b3f9b2
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-04 16:28:12 -0500

    starting to figure out architecture

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const http = require('http');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+app.use(cors);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,9 +21,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4000';
 
 const server = http.createServer(app);
+
+const socketIo = require('socket.io');
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('CONNECTED');
+  socket.emit('fromBackend', 'hello');
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,12 +1,1 @@
-const socketIo = require('socket.io');
-const server = require('../index');
-const io = socketIo(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
 
-io.on('connection', (socket) => {
-  console.log('I am connected!', socket);
-});
