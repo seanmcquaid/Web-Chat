d6192517422102b2b92e4dd743936d3bf760d6cb
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-05 22:08:24 -0500

    set up a quick end point for the user service and basic sketch for a protected route component

--- client/src/api/userService.js
+++ client/src/api/userService.js
@@ -2,3 +2,6 @@ import apiClient from './index';
 
 export const login = (config, { username, password }) =>
   apiClient.post('/users/login', { username, password }, config);
+
+export const register = (config, { username, password }) =>
+  apiClient.post('/users/register', { username, password }, config);
