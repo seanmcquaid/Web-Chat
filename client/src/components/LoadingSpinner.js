a262ee01a0fe5211df203070cdbce43cfa052121
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-06 23:40:31 -0500

    added in loading spinner

---
+++ client/src/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+import { memo } from 'react';
+import { ClimbingBoxLoader } from 'react-spinners';
+import styled from 'styled-components';
+
+const LoadingSpinner = memo(styled(ClimbingBoxLoader)``);
+
+export default LoadingSpinner;
---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import { H1, H2, P } from './Typography';
+import Button from './Button';
+import TextInput from './TextInput';
+import LoadingSpinner from './LoadingSpinner';
+
+export { H1, H2, P, Button, TextInput, LoadingSpinner };
