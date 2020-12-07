3cec99438c82ddf20358b6ac3f5444aab9e139ea
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-06 20:40:41 -0500

    set up users router

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,11 @@
 exports.postLogin = async (req, res, next) => {};
 
 exports.postRegister = async (req, res, next) => {};
+
+exports.getProfile = async (req, res, next) => {};
+
+exports.putProfile = async (req, res, next) => {};
+
+exports.deleteFriend = async (req, res, next) => {};
+
+exports.postFriend = async (req, res, next) => {};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const checkAuth = require('../middleware/checkAuth');
 
 router
   .post('/login', usersController.login)
   .post('/register', usersController.register)
-  .get('/profile/:id')
-  .put('/profile/:id')
-  .delete('/friends/:id')
-  .post('/addFriend');
+  .get('/profile/:id', checkAuth, usersController.getProfile)
+  .put('/profile/:id', checkAuth, usersController.putProfile)
+  .delete('/friends/:id', checkAuth, usersController.deleteFriend)
+  .post('/addFriend', checkAuth, usersController.postFriend);
 
 module.exports = router;
