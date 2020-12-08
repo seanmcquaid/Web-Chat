4398d7f148f73b5957100cc2e3308d7a3aa4c4c3
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-07 21:33:17 -0500

    small code edit for home page

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,21 +1,19 @@
 import styled from 'styled-components';
 import { H1, LinkButton } from '../../components';
 
-const Home = () => {
-  return (
-    <PageContainer>
-      <Header>
-        <H1>Web Chat</H1>
-      </Header>
-      <Main>
-        <LinksContainer>
-          <LinkButton to='/login'>Login</LinkButton>
-          <LinkButton to='/register'>Register</LinkButton>
-        </LinksContainer>
-      </Main>
-    </PageContainer>
-  );
-};
+const Home = () => (
+  <PageContainer>
+    <Header>
+      <H1>Web Chat</H1>
+    </Header>
+    <Main>
+      <LinksContainer>
+        <LinkButton to='/login'>Login</LinkButton>
+        <LinkButton to='/register'>Register</LinkButton>
+      </LinksContainer>
+    </Main>
+  </PageContainer>
+);
 
 const PageContainer = styled.div``;
 
