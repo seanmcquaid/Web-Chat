0dbf344ee83b2ffddc4b9a3428e397361ec61b1f
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-07 21:06:19 -0500

    finished login end point

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,7 +2,32 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const UserModel = require('../models/user');
 
-exports.postLogin = async (req, res, next) => {};
+exports.postLogin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const userInfo = await UserModel.findOne({ username });
+
+    if (!userInfo) {
+      return res.status(401).send({
+        errorMessage: 'No user exists for this username, please try again!',
+      });
+    }
+
+    const isSamePassword = await bcrypt.compare(userInfo.password, password);
+
+    if (!isSamePassword) {
+      return res.status(401).send({
+        errorMessage: "The passwords don't match, please try again!",
+      });
+    }
+
+    return res.status(200).send({ ...userInfo });
+  } catch (error) {
+    return res.status(500).send({
+      errorMessage: 'There was an issue with logging in, please try again!',
+    });
+  }
+};
 
 exports.postRegister = async (req, res, next) => {
   try {
@@ -26,26 +51,41 @@ exports.postRegister = async (req, res, next) => {
 
     return res.status(200).send({ ...userInfo });
   } catch (error) {
-    return res.status(403).send({
+    return res.status(500).send({
       errorMessage: 'There was an issue with registering, please try again!',
     });
   }
 };
 
 exports.getProfile = async (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
+  } catch (error) {}
 };
 
 exports.putProfile = async (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
+  } catch (error) {}
 };
 
 exports.deleteFriend = async (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
+  } catch (error) {}
 };
 
-exports.postFriend = async (req, res, next) => {};
+exports.postFriend = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
 
-exports.getMessages = async (req, res, next) => {};
+exports.getMessages = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
 
-exports.postMessage = async (req, res, next) => {};
+exports.postMessage = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
