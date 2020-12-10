a8ff087b9003cc684e42d216c1efff34f9c66556
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-09 23:06:00 -0500

    added in other containers to work on

--- client/src/containers/UserHome/index.js
+++ client/src/containers/UserHome/index.js
@@ -1,19 +1,27 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { H1, LinkButton } from '../../components';
+import socket from '../../sockets';
 
-const UserHome = () => (
-  <PageContainer>
-    <Header>
-      <H1>Web Chat Home</H1>
-    </Header>
-    <Main>
-      <LinksContainer>
-        <LinkButton to='/'></LinkButton>
-        <LinkButton to='/'></LinkButton>
-      </LinksContainer>
-    </Main>
-  </PageContainer>
-);
+const UserHome = () => {
+  useEffect(() => {
+    socket.on('connection', (data) => {});
+    return () => {};
+  }, []);
+  return (
+    <PageContainer>
+      <Header>
+        <H1>Web Chat Home</H1>
+      </Header>
+      <Main>
+        <LinksContainer>
+          <LinkButton to='/'>User Search</LinkButton>
+          <LinkButton to='/'>Friends List</LinkButton>
+        </LinksContainer>
+      </Main>
+    </PageContainer>
+  );
+};
 
 const PageContainer = styled.div``;
 
