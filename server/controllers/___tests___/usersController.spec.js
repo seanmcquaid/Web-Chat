5d960f489601b3ea968c32a9e29c71b02274f960
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-12 14:27:18 -0500

    finished post login tests

--- server/controllers/___tests___/usersController.spec.js
+++ server/controllers/___tests___/usersController.spec.js
@@ -3,6 +3,8 @@ const mockResponse = require('../../testUtils.js/mockResponse');
 const mockNext = require('../../testUtils.js/mockNext');
 const usersController = require('../usersController');
 const UserModel = require('../../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 describe('usersController', () => {
   describe('postLogin', () => {
@@ -21,9 +23,70 @@ describe('usersController', () => {
       });
     });
 
-    it("Passwords don't match", () => {});
+    it("Passwords don't match", async () => {
+      const body = {
+        username: 'testUser',
+        password: 'testPassword',
+      };
+      const req = mockRequest(body);
+      const res = mockResponse();
+      const next = mockNext();
 
-    it('Valid user logs in', () => {});
+      const userInfo = {
+        _id: 1,
+        username: 'testUser',
+        password: 'testPassword1',
+        isTyping: false,
+        isOnline: true,
+        friends: [],
+        messages: [],
+      };
+
+      jest.spyOn(UserModel, 'findOne').mockImplementationOnce(() => userInfo);
+
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false);
+
+      await usersController.postLogin(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.send).toHaveBeenCalledWith({
+        errorMessage: "The passwords don't match, please try again!",
+      });
+    });
+
+    it('Valid user logs in', async () => {
+      const body = {
+        username: 'testUser',
+        password: 'testPassword',
+      };
+      const req = mockRequest(body);
+      const res = mockResponse();
+      const next = mockNext();
+
+      const userInfo = {
+        _id: 1,
+        username: 'testUser',
+        password: 'testPassword',
+        isTyping: false,
+        isOnline: true,
+        friends: [],
+        messages: [],
+      };
+
+      jest.spyOn(UserModel, 'findOne').mockImplementationOnce(() => userInfo);
+
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => true);
+
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => 'Valid Token');
+
+      await usersController.postLogin(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith({
+        token: 'Valid Token',
+        isOnline: true,
+      });
+    });
   });
 
   describe('postRegister', () => {
