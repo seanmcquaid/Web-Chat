a840539b399168fee5b8709a07d24b14eab0fdef
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-11 22:17:59 -0500

    decided not to use a socket on user home as that's not appropriate

--- client/src/containers/UserHome/index.js
+++ client/src/containers/UserHome/index.js
@@ -3,25 +3,19 @@ import styled from 'styled-components';
 import { H1, LinkButton } from '../../components';
 import socket from '../../sockets';
 
-const UserHome = () => {
-  useEffect(() => {
-    socket.on('connection', (data) => {});
-    return () => {};
-  }, []);
-  return (
-    <PageContainer>
-      <Header>
-        <H1>Web Chat Home</H1>
-      </Header>
-      <Main>
-        <LinksContainer>
-          <LinkButton to='/'>User Search</LinkButton>
-          <LinkButton to='/'>Friends List</LinkButton>
-        </LinksContainer>
-      </Main>
-    </PageContainer>
-  );
-};
+const UserHome = () => (
+  <PageContainer>
+    <Header>
+      <H1>Web Chat Home</H1>
+    </Header>
+    <Main>
+      <LinksContainer>
+        <LinkButton to='/'>User Search</LinkButton>
+        <LinkButton to='/'>Friends List</LinkButton>
+      </LinksContainer>
+    </Main>
+  </PageContainer>
+);
 
 const PageContainer = styled.div``;
 
