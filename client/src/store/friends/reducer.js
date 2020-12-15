7e74ddbb7ab4659ba213bd2b3ec3824b1014d31f
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-14 21:17:41 -0500

    added in reducer logic for add friend success

--- client/src/store/friends/reducer.js
+++ client/src/store/friends/reducer.js
@@ -1,9 +1,16 @@
+import { ADD_FRIEND_SUCCESS } from './types';
+
 const initialState = {
   friendsList: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_FRIEND_SUCCESS:
+      return {
+        ...state,
+        friendsList: [...action.friends],
+      };
     default:
       return state;
   }
