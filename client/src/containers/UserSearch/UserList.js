5b2d516d97cbb7d1d3e95297ede1effcd6ce6422
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-14 21:41:38 -0500

    added in pagination

--- client/src/containers/UserSearch/UserList.js
+++ client/src/containers/UserSearch/UserList.js
@@ -13,6 +13,7 @@ const UserList = memo(({ users }) => {
     () => users.slice(currentPage * 5, (currentPage + 1) * 5),
     [users, currentPage]
   );
+  console.log(displayedUsers, currentPage);
 
   const addFriendButtonOnClick = useCallback(
     (name) => {
@@ -21,30 +22,63 @@ const UserList = memo(({ users }) => {
     [dispatch]
   );
 
+  const decrementPageButtonOnClick = useCallback(() => {
+    setCurrentPage((pageNumber) => pageNumber - 1);
+  }, []);
+
+  const incrementPageButtonOnClick = useCallback(() => {
+    setCurrentPage((pageNumber) => pageNumber + 1);
+  }, []);
+
   if (users.length === 0) {
     return <div>No Users found</div>;
   }
 
   return (
-    <StyledUserList>
-      {displayedUsers.map(({ username, _id }) => (
-        <User key={_id}>
-          {username}{' '}
-          <Button
-            onClick={() => addFriendButtonOnClick(username)}
-            type='button'
-          >
-            Add Friend
-          </Button>
-        </User>
-      ))}
-    </StyledUserList>
+    <>
+      <StyledUserList>
+        {displayedUsers.map(({ username, _id }) => (
+          <User key={_id}>
+            {username}{' '}
+            <Button
+              onClick={() => addFriendButtonOnClick(username)}
+              type='button'
+            >
+              Add Friend
+            </Button>
+          </User>
+        ))}
+      </StyledUserList>
+      <ListFooter>
+        <Button
+          type='button'
+          onClick={decrementPageButtonOnClick}
+          disabled={currentPage === 0}
+        >
+          Prev Page
+        </Button>
+        <PageText>
+          {currentPage + 1} of {totalPages}
+        </PageText>
+        <Button
+          type='button'
+          onClick={incrementPageButtonOnClick}
+          disabled={currentPage + 1 === totalPages}
+        >
+          Next Page
+        </Button>
+      </ListFooter>
+    </>
   );
 });
 
 const StyledUserList = styled.ul``;
 
 const User = styled.li``;
+
+const ListFooter = styled.div``;
+
+const PageText = styled.span``;
 
 UserList.propTypes = {
   users: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
