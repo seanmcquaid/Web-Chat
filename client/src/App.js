79fdd9c54e21683eacbcf39695298f25f474b480
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-19 12:54:23 -0500

    added in nav to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Navbar } from './components';
 import Routes from './routes';
 import configureStore from './store';
 
@@ -9,6 +12,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Navbar />
         <Routes />
       </PersistGate>
     </Provider>
