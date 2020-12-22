7506ba0c24342b18718456a5631085549abeeea5
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-22 13:21:51 -0500

    finished user actions tests

--- client/src/store/user/___tests___/actions.spec.js
+++ client/src/store/user/___tests___/actions.spec.js
@@ -1,6 +1,11 @@
 import * as userService from '../../../api/userService';
 import createMockStore from '../../../testUtils/createMockStore';
-import { loginAction } from '../actions';
+import {
+  getUserInfoAction,
+  loginAction,
+  logoutAction,
+  registerAction,
+} from '../actions';
 
 describe('userActions', () => {
   it('loginAction', () => {
@@ -18,6 +23,64 @@ describe('userActions', () => {
     ];
 
     return store.dispatch(loginAction('Username here', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('registerAction', () => {
+    const store = createMockStore();
+    jest.spyOn(userService, 'register').mockResolvedValueOnce({
+      data: { username: 'Username here', friends: [] },
+    });
+
+    const expectedActions = [
+      { type: 'REGISTER_LOADING' },
+      {
+        type: 'REGISTER_SUCCESS',
+        userInfo: { username: 'Username here', friends: [] },
+      },
+    ];
+
+    return store
+      .dispatch(registerAction('Username here', 'password'))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+  });
+
+  it('getUserInfoAction', () => {
+    const store = createMockStore({ user: { token: 'Valid token' } });
+    jest.spyOn(userService, 'getUserInfo').mockResolvedValueOnce({
+      data: { username: 'Username here', friends: [] },
+    });
+
+    const expectedActions = [
+      { type: 'GET_USER_INFO_LOADING' },
+      {
+        type: 'GET_USER_INFO_SUCCESS',
+        userInfo: { username: 'Username here', friends: [] },
+      },
+    ];
+
+    return store.dispatch(getUserInfoAction()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('logoutAction', () => {
+    const store = createMockStore({ user: { token: 'Valid token' } });
+    jest.spyOn(userService, 'logout').mockResolvedValueOnce({
+      data: { username: 'Username here', friends: [] },
+    });
+
+    const expectedActions = [
+      { type: 'LOGOUT_LOADING' },
+      {
+        type: 'LOGOUT_SUCCESS',
+      },
+    ];
+
+    return store.dispatch(logoutAction()).then(() => {
       expect(store.getActions()).toEqual(expectedActions);
     });
   });
