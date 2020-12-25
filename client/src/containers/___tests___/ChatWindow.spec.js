bc207a1f52d8d28e2c7e32ec7c318e2f167394e3
seanmcquaid <sean.mcquaid2@verizon.net>
2020-12-25 13:21:04 -0500

    just set up the actual rendering for chat window testing, need to do some research on mocking socket io properly

--- client/src/containers/___tests___/ChatWindow.spec.js
+++ client/src/containers/___tests___/ChatWindow.spec.js
@@ -1,5 +1,58 @@
-describe('<ChatWindow/>', () => {
-  it('Friend is typing displays', () => {});
+import { render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { Switch } from 'react-router-dom';
+import * as userService from '../../api/userService';
+import ProtectedRoute from '../../routes/ProtectedRoute';
+import configureStore from '../../store';
+import MockRouter from '../../testUtils/MockRouter';
+import ChatWindow from '../ChatWindow';
+import socket from '../../sockets';
+import { RECEIVE_IS_FRIEND_TYPING } from '../../sockets/types';
 
-  it('Sent message displays', () => {});
+describe('<ChatWindow/>', () => {
+  it('Friend is typing displays', async () => {
+    const initialState = {
+      user: {
+        token: 'valid token',
+      },
+    };
+    const { store } = configureStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <MockRouter initialRoute='/chatWindow/sean.mcquaid'>
+          <Switch>
+            <ProtectedRoute
+              exact
+              path='/chatWindow/:name'
+              component={ChatWindow}
+            />
+          </Switch>
+        </MockRouter>
+      </Provider>
+    );
+  });
+
+  it('Sent message displays', () => {
+    const initialState = {
+      user: {
+        token: 'valid token',
+      },
+    };
+    const { store } = configureStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <MockRouter initialRoute='/chatWindow/sean.mcquaid'>
+          <Switch>
+            <ProtectedRoute
+              exact
+              path='/chatWindow/:name'
+              component={ChatWindow}
+            />
+          </Switch>
+        </MockRouter>
+      </Provider>
+    );
+  });
 });
